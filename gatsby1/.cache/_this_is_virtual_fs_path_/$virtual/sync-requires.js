3f43cccfce7892c4aa3e22b0b824f815cd87f0b9
Tarun Kumar <tarun@Taruns-MacBook-Air.local>
2021-06-25 10:47:47 +0530

    commit

---
+++ gatsby1/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/src/pages/about.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/src/pages/page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/tarun/projects/gatsby-sanity/gatsby1/src/pages/using-typescript.tsx"))
+}
+
